12d6d521e4efb4123326faea1765f7e6af0e3fcc
Dominik Hauser <dom@dasdom.de>
2015-12-29 20:30:38 +0100

    Initial commit

--- ResponderChainDemo/ViewController.swift
+++ ResponderChainDemo/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
+class ViewWithButton: UIView {
+    
+    let button: UIButton
+    
+    override init(frame: CGRect) {
+        button = UIButton(type: .System)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("The Button", forState: .Normal)
+        button.addTarget(nil, action: "onButtonTap:", forControlEvents: .TouchUpInside)
+        
+        super.init(frame: frame)
+        
+        backgroundColor = .yellowColor()
+        
+        addSubview(button)
+        
+        button.centerXAnchor.constraintEqualToAnchor(centerXAnchor).active = true
+        button.centerYAnchor.constraintEqualToAnchor(centerYAnchor).active = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
 class ViewController: UIViewController {
+    
+    let viewWithButton = ViewWithButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.addSubview(viewWithButton)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func onButtonTap(sender: UIButton) {
+        print("This button was clicked in the subview!")
     }
-
-
 }
 
